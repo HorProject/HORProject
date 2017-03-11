e39b6ae461093c0dbf6d53395bffeefc7f91940c
Boat <boat46450@hotmail.com>
2017-03-11 13:41:27 +0700

    add method in database.java

--- HelpDesk/src/helpdesk/database/Database.java
+++ HelpDesk/src/helpdesk/database/Database.java
@@ -2,9 +2,9 @@ package helpdesk.database;
 
 import java.sql.*;
 public class Database {
-    Connection connect;
-    PreparedStatement ps;
-    ResultSet result;
+    private Connection connect;
+    private Statement stmt;
+    private ResultSet rs;
     
     public Database() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
@@ -12,16 +12,32 @@ public class Database {
     
     public void connect() throws Exception {
         connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/abc?useUnicode=true&characterEncoding=UTF-8","smit","admin");
+        stmt = connect.createStatement();
     }
     public void statement(String select) throws Exception {
-        ps = connect.prepareStatement(select);
-        result = ps.executeQuery();
+        rs = stmt.executeQuery(select);
     }
-    public String getProblem(){
-        return "";
+    public String getUsername() throws Exception {
+        return rs.getString("usernane");
+    }
+    public String getPassword() throws Exception {
+        return rs.getString("password");
+    }
+    public String getHName() throws Exception {
+        return rs.getString("hName");
+    }
+    public String getRoom() throws Exception {
+        return rs.getString("room");
+    }
+    public String[] getProblem(){
+        String[] problem = new String[27];
+        return problem;
+    }
+    public void nextRow() throws Exception {
+        rs.next();
     }
     public void close() throws Exception {
-        ps.close();
+        rs.close();
         connect.close();
     }
 }
