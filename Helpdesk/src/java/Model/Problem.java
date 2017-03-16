d9a1c65605b1397252942a6e8c0165682ee7b57a
Boat <boat46450@hotmail.com>
2017-03-17 00:32:45 +0700

    before forget save

--- Helpdesk/src/java/Model/Problem.java
+++ Helpdesk/src/java/Model/Problem.java
@@ -10,6 +10,38 @@ public class Problem {
     public Problem(int causeId) {
         this.causeId = causeId;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProblemP() {
+        return problemP;
+    }
+
+    public void setProblemP(String problemP) {
+        this.problemP = problemP;
+    }
+
+    public String getProblemN() {
+        return problemN;
+    }
+
+    public void setProblemN(String problemN) {
+        this.problemN = problemN;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
     
 }
