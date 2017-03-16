3075d53f95725c88e68fcb1f1673c3591e356c62
Boat <boat46450@hotmail.com>
2017-03-17 00:04:41 +0700

    various change - ConnectionBuilder edit user&pass and change try...catch to throws - Problem add attribute,constructor,getter,setter - ShowListProblem add attribute, constructor

--- Helpdesk/src/java/Model/ConnectionBuilder.java
+++ Helpdesk/src/java/Model/ConnectionBuilder.java
@@ -3,16 +3,9 @@ package Model;
 import java.sql.*;
 
 public class ConnectionBuilder {
-    public static Connection getConnection() throws Exception {
+    public static Connection getConnection() throws SQLException {
         Connection con = null;
-        try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/horsql","root","");
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        finally {
-            return con;
-        }
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/horsql","home","0123456789");
+        return con;
     }
 }
--- Helpdesk/src/java/Model/Problem.java
+++ Helpdesk/src/java/Model/Problem.java
@@ -1,4 +1,15 @@
 package Model;
 
 public class Problem {
+    private int causeId;
+    private String name;
+    private String problemP;
+    private String problemN;
+    private String status;
+
+    public Problem(int causeId) {
+        this.causeId = causeId;
+    }
+    
+    
 }
