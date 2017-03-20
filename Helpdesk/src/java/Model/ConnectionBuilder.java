4f9ee27e6c15063607c1bf20b135be0910d89327
Boat <boat46450@hotmail.com>
2017-03-20 21:47:17 +0700

    change db form local to online

--- Helpdesk/src/java/Model/ConnectionBuilder.java
+++ Helpdesk/src/java/Model/ConnectionBuilder.java
@@ -3,9 +3,13 @@ package Model;
 import java.sql.*;
 
 public class ConnectionBuilder {
+    private static final String host = "sql12.freemysqlhosting.net";
+    private static final String dbName = "sql12164714";
+    private static final String username = "sql12164714";
+    private static final String password = "KkiSNdavSE";
     public static Connection getConnection() throws SQLException {
         Connection con = null;
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/horsql","home","0123456789");
+        con = DriverManager.getConnection("jdbc:mysql://" + host + ":3306/" + dbName, username , password);
         return con;
     }
 }
