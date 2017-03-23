c7904fc931adaa0f81333cb39c12418fd2d4a228
permupop <vidadevil31@gmail.com>
2017-03-23 17:49:14 +0700

    add  Arraylist

--- HelpDesk/src/java/Model/ReportProblem.java
+++ HelpDesk/src/java/Model/ReportProblem.java
@@ -1,5 +1,7 @@
 package Model;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class ReportProblem {
     private long userId;
@@ -8,9 +10,10 @@ public class ReportProblem {
     private int dormId;
     private String name;
     private String surname;
+    private ArrayList<Problem> list;
     
     public ReportProblem(long userId){    
-           this.userId = userId;
+        this.userId = userId;
         try{
             Connection connect = ConnectionBuilder.getConnection();
             PreparedStatement ps = connect.prepareStatement("SELECT  user.userFirstName , user.userLastName FROM user WHERE userId = ?");
@@ -21,13 +24,34 @@ public class ReportProblem {
                 surname = result.getString("userLastName");
             }
             ps.close();
+            ps = connect.prepareStatement(
+                    "SELECT user.userId, room.roomId, room.roomNo, room.Dormitory_dormId "
+                            + "FROM user "
+                            + "INNER JOIN renter ON user.userId = renter.User_userId "
+                            + "INNER JOIN renter_has_room ON renter.renterId = renter_has_room.Renter_renterId "
+                            + "INNER JOIN room ON renter_has_room.Room_roomId = room.roomId "
+                            + "WHERE userId = ?");
+            ps.setLong(1,userId);
+            result = ps.executeQuery();
+            while(result.next()){
+                roomId = result.getInt("roomId");
+                roomNo = result.getInt("roomNo");
+                dormId = result.getInt("Dormitory_dormId");
+            }
+            ps.close();
+            connect.close();
         }
         catch(SQLException e) {
             e.printStackTrace();
         }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        list = new ListProblem(userId).getList();
+    }   
     
-    }    
-    public void report(int userId){
+    public void report(int problemId){
+        
         
             
             
